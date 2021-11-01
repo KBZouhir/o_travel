65fdeafec45d3e451fa50e7ce79d7925b04c5abf
Zouhir <kouiderbouabdalahzouhir@gmail.com>
2021-11-01 14:34:18 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/otravel/o_travel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.otravel.o_travel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/CONFIG.dart
@@ -0,0 +1,8 @@
+
+
+//const String BASE_DOMAINE="192.168.1.39:8000";
+const String BASE_DOMAINE="mkshop.meriemk.com";
+const String BASE_DOMAINE_HTTP="http://$BASE_DOMAINE";
+const String BASE_DOMAINE_IMAGE="http://$BASE_DOMAINE/storage/";
+
+const String PRIVACY_POLICY = "$BASE_DOMAINE_HTTP/privacy_policies";
